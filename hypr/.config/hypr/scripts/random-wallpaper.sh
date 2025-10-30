f90ad1febc08651c64ed22a2b68bd8e5738e912b
maurice koreman <mg.koreman99@gmail.com>
2025-10-30 21:05:17 +0100

    add hellwal for ricing waybar with background automatically

--- hypr/.config/hypr/scripts/random-wallpaper.sh
+++ hypr/.config/hypr/scripts/random-wallpaper.sh
@@ -6,3 +6,4 @@ WALLPAPER_DIR="$HOME/dotfiles/backgrounds"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 hyprctl hyprpaper reload ,"$WALLPAPER"
+hellwal -i "$WALLPAPER" --skip-term-colors --bright-offset 0.4
