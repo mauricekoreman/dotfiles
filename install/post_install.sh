c08ce4650d5e88cc0548e7fbe876e79cc42f9748
maurice koreman <mg.koreman99@gmail.com>
2025-11-03 13:57:40 +0100

    added install files, update hypr config

---
+++ install/post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$EUID" == 0 ]]; then
+  echo "Please run this script as a user with sudo privilege."
+  exit 1
+fi
+
+echo "Installing packages from pgklist.txt..."
+sudo pacman -Syu --noconfirm --needed - <pgklist.txt
+
+echo "Enabling essential servives..."
+# sudo systemctl enable NetworkManager
+
+echo "Installation and configuration complete! Reboot is recommended."
